f9d6abe7e228d47813ce97919df3e6cb68c4cc9f
jasulli2 <sulliman@gmail.com>
2019-07-29 16:13:51 +1200

    excel

--- main.py
+++ main.py
@@ -61,52 +61,7 @@ for network in meraki_data['networks']:
     get_network_info(append_url='l3FirewallRules', dict_key='l3FirewallRules')
     get_network_info(append_url='vlans', dict_key='vlans')
 
-    '''
-    # Get Meraki devices
-    devices = http_get(meraki_url='networks/{}/devices'.format(network["id"]))
-    for device in devices:
-        meraki_data["devices"].append(device)
 
-    # Get Security Events
-    sec_events = http_get(meraki_url='networks/{}/securityEvents'.format(network["id"]))
-    for sec_event in sec_events:
-        meraki_data["securityEvents"].append(sec_event)
-
-    # Get SSIDs
-    ssids = http_get(meraki_url='networks/{}/ssids'.format(network["id"]))
-    for ssid in ssids:
-        meraki_data["ssids"].append(ssid)
-
-    # Get Firewall Services
-    fw_services = http_get(meraki_url='networks/{}/firewalledServices'.format(network["id"]))
-    for fwService in fw_services:
-        meraki_data["fwServices"].append(fwService)
-    '''
-    # Get Malware Settings
-    '''
-    malware_settings = http_get(meraki_url='networks/{}/malwareSettings'.format(network["id"]))
-    for malware in malware_settings:
-        meraki_data["malwareSettings"].append(malware)
-
-    # Get L3 Firewall Rules
-    l3_rules = http_get(meraki_url='networks/{}/l3FirewallRules'.format(network["id"]))
-    for rule in l3_rules:
-        meraki_data["l3FirewallRules"].append(rule)
-
-    # Get VLANs
-    vlans = http_get(meraki_url='networks/{}/vlans'.format(network["id"]))
-    for vlan in vlans:
-        meraki_data["vlans"].append(vlan)
-    '''
-
-#pp.pprint(meraki_data)
-
-'''
-w = csv.writer(open("vlans_output.csv", "w"))
-for i in meraki_data['vlans']:
-    for key, val in i.items():
-        w.writerow([key, val])
-'''
 licenses_df = pd.DataFrame.from_dict(meraki_data['licenses'])
 orgs_df = pd.DataFrame.from_dict(meraki_data['orgs'])
 networks_df = pd.DataFrame.from_dict(meraki_data['networks'])
@@ -123,57 +78,3 @@ with pd.ExcelWriter('output.xlsx') as writer:  # doctest: +SKIP
     devices_df.to_excel(writer, sheet_name='devices')
     l3FirewallRules_df.to_excel(writer, sheet_name='l3FirewallRules')
     ssids_df.to_excel(writer, sheet_name='ssids')
-
-print (meraki_data)
-
-'''
-print(orgs[0]["id"])
-networks = aci_get(apic_url='organizations/{}/networks'.format(orgs[0]["id"]))
-org_license = aci_get(apic_url='organizations/{}/licenseState'.format(orgs[0]["id"]))
-print(org_license)
-print(networks)
-devices = aci_get(apic_url='networks/{}/devices'.format(networks[0]["id"]))
-sec_events = aci_get(apic_url='networks/{}/securityEvents'.format(networks[0]["id"]))
-ssids = aci_get(apic_url='networks/{}/ssids'.format(networks[0]["id"]))
-print(ssids)
-
-print(devices)
-firewall_services = aci_get(apic_url='networks/{}/firewalledServices'.format(networks[0]["id"]))
-ips = aci_get(apic_url='networks/{}/security/intrusionSettings'.format(networks[0]["id"]))
-malware = aci_get(apic_url='networks/{}/security/malwareSettings'.format(networks[0]["id"]))
-net_fw = aci_get(apic_url='networks/{}/l3FirewallRules'.format(networks[0]["id"]))
-net_vlans = aci_get(apic_url='networks/{}/vlans'.format(networks[0]["id"]))
-print(net_vlans)
-input('vl??')
-print(net_fw)
-print(malware)
-print(ips)
-print(firewall_services)
-device_serials = []
-for device in devices:
-    device_serials.append(device["serial"])
-    print(device["serial"])
-    input("cio?")
-    #switchports = aci_get(apic_url='/devices/{}/switchPorts'.format(device["serial"]))
-    #print(switchports)
-print(device_serials)
-
-uplinks = []
-clients = []
-
-org_ips = aci_get(apic_url='organizations/{}/security/intrusionSettings'.format(orgs[0]["id"]))
-print(org_ips)
-for serial in device_serials:
-    uplink = aci_get(apic_url='networks/{}/devices/{}/uplink'.format(networks[0]["id"], serial))
-    uplinks.append({serial : uplink})
-    client = aci_get(apic_url='/devices/{}/clients'.format(serial))
-'''
-'''
-    for i in client:
-        traffic = aci_get(apic_url='networks/{}/clients/{}/trafficHistory'.format(networks[0]["id"], i["id"]))
-        time.sleep(1)
-        print(traffic)
-    clients.append({'serial_number' : serial,
-                    'network_id' : networks[0]["id"],
-                    'client_id': client})
-'''
